02995e2dd0fc8b8b3516d77ae8d936eec067247d
Matheus Dalsente Krause <mdkrause@iastate.edu>
2022-09-23 11:21:18 -0500

    Add files via upload

--- CV1_CV2.R
+++ CV1_CV2.R
@@ -1,3 +1,25 @@
+
+########################################################################################################################
+##                                                                                                                    ##
+##                                                                                                                    ##
+##  Date: September, 2022                                                                                             ##
+##                                                                                                                    ##
+##  Boosting predictive ability of tropical maize hybrids via genotype-by-environment interaction under multivariate  ##
+##  GBLUP models                                                                                                      ## 
+##  DOI: 10.1002/csc2.20253                                                                                           ##
+##  Krause et al. (2020)                                                                                              ##
+##                                                                                                                    ##
+##  Author:    MD Krause        <krause.d.matheus@gmail.com>                                                          ##
+##                                                                                                                    ##
+########################################################################################################################
+
+########################################################################################################################
+##                                                  DISCLAIMER                                                        ##
+##             I honestly do not recall If I wrote these functions, modified them, or simply found them               ##
+##         somewhere else. If you identify yourself as the author, please contact me so I can cite you.               ##
+########################################################################################################################
+
+
 SamplingLines <- function(id,I,seed=NULL,rep=NULL){
         if(is.null(seed)) seed <-1010231
         if(is.null(rep)) rep<-10
